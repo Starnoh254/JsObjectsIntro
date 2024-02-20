d0ca5e72db8999968b1c234b3c140af5d7f01df8
Starnoh254 <stajohn697@gmail.com>
2024-02-20 21:57:22 +0300

    Latest changes .

--- classes.js
+++ classes.js
@@ -1,1 +1,33 @@
 // Classes in Js
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  talk() {
+    return `Hello , my name is ${this.name} and I am ${this.age} years old`;
+  }
+}
+
+const stan = new Person("Stan", 20);
+
+console.log(stan.talk());
+
+class Employee extends Person {
+  constructor(name, age, proffession) {
+    super(name, age);
+    this.proffession = proffession;
+  }
+
+  introduction() {
+    return `
+        ${super.talk()}
+         I work as a ${this.proffession}
+         `;
+  }
+}
+
+const john = new Employee("John", 25, "Software Engineer");
+console.log(john.talk())
+console.log(john.introduction());
---
+++ doWhile.js
@@ -0,0 +1,6 @@
+// do - while loop in js
+let i = 0
+do {
+    console.log(i)
+    i++
+}while(i <= 100)
