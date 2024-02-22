95c8a07cb5f9e4d1c47fcdf234a7753d3f500c1a
Starnoh254 <stajohn697@gmail.com>
2024-02-22 20:04:38 +0300

    wrote code for exponents in JS

---
+++ exponent.js
@@ -0,0 +1,15 @@
+/* fuction to get the result of powering a number 
+For example , exponent(2,3) gives you 8 as the result */
+
+
+function exponent (base , exp){
+    if (exp === 0) return 1
+    let result = base;
+    for(let i = 1 ; i < exp; i ++){
+        result = result * base
+        // console.log(i)
+    }
+    return result;
+}
+
+console.log(exponent(7,2))
