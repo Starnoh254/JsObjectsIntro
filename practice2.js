5c4b5fd3abb611aecfcc1e7c4e4b798dd90191b4
Starnoh254 <stajohn697@gmail.com>
2024-03-02 21:43:12 +0300

    latest

---
+++ practice2.js
@@ -0,0 +1,15 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  getFullName() {
+    return `My name is ${this.firstName} ${this.lastName}`;
+  }
+}
+
+const jane = new Person("Jane", "Doe");
+const john = new Person("John", "Doe");
+
+console.log(jane.getFullName());
+console.log(john.getFullName())
